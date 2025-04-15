5efb8b77e24363238bbfe20bbd89fd823f539298
manueeeeeeo <manu.engenios@gamil.com>
2025-04-15 22:13:22 +0200

    Proyecto ya funcional y se puede abrir en el navegador

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -3,7 +3,6 @@
     <h2 class="text-3xl font-semibold mb-4">Proyectos</h2>
     <div class="grid md:grid-cols-2 gap-6">
       <div class="bg-white rounded-xl shadow hover:shadow-lg transition p-4">
-        <img src="/project1.jpg" alt="Proyecto 1" class="rounded mb-2" />
         <h3 class="text-xl font-semibold">Nombre del Proyecto</h3>
         <p class="text-sm text-gray-600">Descripción breve del proyecto.</p>
         <div class="flex justify-between mt-3">
