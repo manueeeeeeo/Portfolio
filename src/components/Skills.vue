c86e42fb266f9e3f88628d587109d97560329599
manueeeeeeo <manu.engenios@gamil.com>
2025-04-15 21:59:27 +0200

    Agregadaos iconos para las pruebas y cambios de nomrbes de archivos

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -3,15 +3,15 @@
     <h2 class="text-3xl font-semibold mb-4">Habilidades</h2>
     <div class="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
       <div class="bg-white p-4 rounded shadow text-center">
-        <img src="/icons/vue.svg" alt="Vue.js" class="w-10 h-10 mx-auto mb-2" />
+        <img src="/icons/vue.png" alt="Vue.js" class="w-10 h-10 mx-auto mb-2" />
         <p class="text-sm">Vue.js</p>
       </div>
       <div class="bg-white p-4 rounded shadow text-center">
-        <img src="/icons/tailwind.svg" alt="Tailwind CSS" class="w-10 h-10 mx-auto mb-2" />
+        <img src="/icons/tailwind.png" alt="Tailwind CSS" class="w-10 h-10 mx-auto mb-2" />
         <p class="text-sm">Tailwind CSS</p>
       </div>
       <div class="bg-white p-4 rounded shadow text-center">
-        <img src="/icons/js.svg" alt="JavaScript" class="w-10 h-10 mx-auto mb-2" />
+        <img src="/icons/js.png" alt="JavaScript" class="w-10 h-10 mx-auto mb-2" />
         <p class="text-sm">JavaScript</p>
       </div>
     </div>
