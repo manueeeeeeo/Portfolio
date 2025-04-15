36ec1bc993bd295173c5545e906acf5b72f19766
manueeeeeeo <manu.engenios@gamil.com>
2025-04-15 20:27:29 +0200

    Mas cambios en cuanto a los componentes Vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,9 @@
 <template>
-    <footer class="text-center p-4 text-sm text-gray-500 border-t mt-12">
-      <p>© {{ new Date().getFullYear() }} Manuel. Todos los derechos reservados.</p>
-      <div class="flex justify-center gap-4 mt-2 text-xl">
-        <a href="https://github.com/tuusuario" target="_blank" class="hover:text-gray-700">GitHub</a>
-        <a href="https://linkedin.com/in/tuusuario" target="_blank" class="hover:text-gray-700">LinkedIn</a>
-      </div>
-    </footer>
-  </template>
-  
+  <footer class="text-center p-4 text-sm text-gray-500 border-t mt-12">
+    <p>© {{ new Date().getFullYear() }} Manuel. Todos los derechos reservados.</p>
+    <div class="flex justify-center gap-4 mt-2 text-xl">
+      <a href="https://github.com/tuusuario" target="_blank" class="hover:text-gray-700">GitHub</a>
+      <a href="https://linkedin.com/in/tuusuario" target="_blank" class="hover:text-gray-700">LinkedIn</a>
+    </div>
+  </footer>
+</template>
